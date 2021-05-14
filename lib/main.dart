01b663ffc8b3be713fad92b0f6b975d56146cb78
naresh20101 <khatrinaresh20101@gmail.com>
2021-05-14 17:02:20 +0500

    Practice

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Text("Naresh Kumar"),
+      ),
+    );
+  }
+}
+
